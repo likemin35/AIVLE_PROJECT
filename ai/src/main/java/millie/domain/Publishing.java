8e2f1728f38b02d85b0254e7cea9efddf53121fc
김영진 <109127043+dudwls3550@users.noreply.github.com>
2025-07-01 15:12:38 +0900

    Update Publishing.java

--- ai/src/main/java/millie/domain/Publishing.java
+++ ai/src/main/java/millie/domain/Publishing.java
@@ -11,20 +11,27 @@ import javax.persistence.*;
 @Entity
 @Table(name = "Publishing_table")
 @Data
+
 public class Publishing {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    
     private Long id;
 
+    @Column
     private String image;             // AI 생성 표지 이미지 URL
+    @Column
     private String summaryContent;    // 요약 내용
     private String bookName;          // 제목
     private String pdfPath;           // PDF 경로
     private String authorId;          // 저자 ID
     private String webUrl;            // 웹 주소
     private String category;          // 장르 (소설, 에세이 등)
+    @Column
     private Integer cost;             // 가격 (연속형)
+    private String title;
+    private String content;
 
     @Embedded
     private ManuscriptId manuscriptId;
