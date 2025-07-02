d70ac1be0f7584d3a4825e6149d506f040983b7f
jeh0103 <166023112+jeh0103@users.noreply.github.com>
2025-07-02 11:05:24 +0900

    Update AuthorId.java

--- writing/src/main/java/millie/domain/AuthorId.java
+++ writing/src/main/java/millie/domain/AuthorId.java
@@ -8,29 +8,17 @@ import java.util.Objects;
 public class AuthorId implements Serializable {
 
     private String id;
-    private boolean isApprove;
 
     protected AuthorId() {
         this.id = null;
-        this.isApprove = false;
     }
 
     public AuthorId(String id) {
         this.id = id;
-        this.isApprove = false;
-    }
-
-    public AuthorId(String id, boolean isApprove) {
-        this.id = id;
-        this.isApprove = isApprove;
     }
 
     public String getId() {
         return id;
-    }
-
-    public boolean isApprove() {
-        return isApprove;
     }
 
     @Override
@@ -38,11 +26,11 @@ public class AuthorId implements Serializable {
         if (this == o) return true;
         if (!(o instanceof AuthorId)) return false;
         AuthorId authorId = (AuthorId) o;
-        return isApprove == authorId.isApprove && Objects.equals(id, authorId.id);
+        return Objects.equals(id, authorId.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, isApprove);
+        return Objects.hash(id);
     }
 }
