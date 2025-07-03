f5c8be31bf48f3a7139598f9cd26796e9067f0f3
김영진 <109127043+dudwls3550@users.noreply.github.com>
2025-07-03 11:18:28 +0900

    Update PublishingRepository.java

--- ai/src/main/java/millie/domain/PublishingRepository.java
+++ ai/src/main/java/millie/domain/PublishingRepository.java
@@ -1,13 +1,22 @@
 package millie.domain;
 
+import java.util.*;
+import lombok.*;
 import millie.domain.*;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import millie.infra.AbstractEvent;
 
-//<<< PoEAA / Repository
-@RepositoryRestResource(
-    collectionResourceRel = "publishings",
-    path = "publishings"
-)
-public interface PublishingRepository
-    extends PagingAndSortingRepository<Publishing, Long> {}
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PublishingRequested extends AbstractEvent {
+
+    private Long id;
+    private String title;
+    private Object authorId;
+    private Object status;
+    private String content;
+
+    private String category;
+    private String webUrl;
+}
